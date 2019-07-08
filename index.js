9a30361476da64832fc0ba1c5c9e61caed246c0c
oleg711-lab <ya.door2017@yandex.ru>
2019-07-08 18:00:04 +0400

    first

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
 const TOKEN= '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pVMc'
+
+const bot =new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'hello fom j,bot says:"hi, ${msg.from.first_name}"')
+
+} )
