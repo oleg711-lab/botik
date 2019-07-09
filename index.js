2452119bab22af90166be400dd328adc575c0e3b
oleg711-lab <52378082+oleg711-lab@users.noreply.github.com>
2019-07-09 17:07:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-var TelegramBot = require('node-telegram-bot-api');
- 
-var token = '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pVMc';
-var botOptions = {
-    polling: true
-};
-var bot = new TelegramBot(token, botOptions);
- 
-bot.getMe().then(function(me)
-{
-    console.log('Hello! My name is %s!', me.first_name);
-    console.log('My id is %s.', me.id);
-    console.log('And my username is @%s.', me.username);
-});
- 
-bot.on('text', function(msg)
-{
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUsr = msg.from.username;
- 
-    if (messageText === '/say') {
-        sendMessageByBot(messageChatId, 'Hello World!');
-    }
- 
-    console.log(msg);
-});
- 
-function sendMessageByBot(aChatId, aMessage)
-{
-    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+const TelegramBot = require('node-telegram-bot-api')
+const TOKEN= '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pVMc'
+
+const bot =new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'hello fom j,bot says:"hi, ${msg.from.first_name}"')
+
+})
