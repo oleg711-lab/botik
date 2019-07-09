5370b408b758081ac94fbbb15541bd2a8a8b7bed
oleg711-lab <52378082+oleg711-lab@users.noreply.github.com>
2019-07-09 16:43:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-
-var telegram = require('telegram-bot-api');
-
-var api = new telegram({
-    token: '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pVMc',
-    updates: {
-                enabled: true,
-                get_interval: 1000
-             }
-});
-api.on('message', function(message)
+var TelegramBot = require('node-telegram-bot-api');
+ 
+var token = '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pVMc';
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
 {
-    var chat_id = message.chat.id;
-        console.log("This is the user's chat id"+chat_id);
-
-api.sendPhoto({
-    chat_id : message.chat.id,
-    caption: 'This is my test image',
-    photo: 'https://telegram-store.com/wp-content/uploads/2016/11/Kak-skopirovat-ssylku-v-Telegramme-dlya-priglasheniya-polzovatelej.png'//replace your image url here    
-})
-.then(function(data)
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
+bot.on('text', function(msg)
 {
-    console.log(data);
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
 });
-});
-  
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
