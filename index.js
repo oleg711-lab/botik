40f4af8a58d3022b2678c23a1b765cca2cb42585
oleg711-lab <52378082+oleg711-lab@users.noreply.github.com>
2019-07-09 15:57:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, 'hello fom j,bot says:"hi, ${msg.from.first_name}"')
 
 } )
+} )
+bot <-  Bot (token  =  bot_token ("RTelegramBot"))
+chat_id < -  user_id ("Me")
+photo_url < -  "https://telegram.org/img/t_logo.png"
+
+bot $ sendPhoto (
+  chat_id  =  chat_id,
+  photo  =  photo_url,
+  caption  =  "Telegram Logo"
+) 
