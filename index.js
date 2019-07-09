40e112fae65433f52ba596ca4a63fc35ba1b5e87
oleg711-lab <52378082+oleg711-lab@users.noreply.github.com>
2019-07-09 16:12:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const bot =new TelegramBot(TOKEN, {polling: true})
 bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, 'hello fom j,bot says:"hi, ${msg.from.first_name}"')
 
-} )
-} )
-bot <-  Bot (token  =  bot_token ("RTelegramBot"))
+}
+{
+  bot <-  Bot (token  =  bot_token ("RTelegramBot"))
 chat_id < -  user_id ("Me")
 photo_url < -  "https://telegram.org/img/t_logo.png"
 
@@ -16,4 +16,5 @@ bot $ sendPhoto (
   chat_id  =  chat_id,
   photo  =  photo_url,
   caption  =  "Telegram Logo"
-) 
+}
+)
