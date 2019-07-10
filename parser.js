c8da69ae9c2868b773d1357236aabf2bd56025ad
oleg711-lab <52378082+oleg711-lab@users.noreply.github.com>
2019-07-10 07:32:31 +0300

    Update and rename parser to parser.js

--- parser.js
+++ parser.js
@@ -1,14 +1,14 @@
 var telegram = require('telegram-bot-api');
 
 var api = new telegram({
-    token: '< 833678201:aah0zdprbcei4jn475wl4zql0kv84s6pv>',
+    token: '<833678201:aah0zdprbcei4jn475wl4zql0kv84s6pv>',
 });
 
 api.sendPhoto({
     chat_id: <YOUR CHAT ID>,
     caption: 'This is my test image',
 
-    // you can also send file_id here as string (as described in telegram bot api documentation)
+   you can also send file_id here as string (as described in telegram bot api documentation)
     photo: 'https://core.telegram.org/file/811140217/1/NkRCCLeQZVc/17a804837802700ea4'
 })
 .then(function(data)
