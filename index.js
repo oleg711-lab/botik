c26ed96c1dfc4bb4fd3fd2f69fd92a8abfd7d780
oleg711-lab <52378082+oleg711-lab@users.noreply.github.com>
2019-07-10 17:52:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const TelegramBot = require('node-telegram-bot-api')
 const TOKEN= '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pVMc'
 
 const bot =new TelegramBot(TOKEN, {polling: true})
+var telegram =  require ('telegram-bot-api'));
 
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'hello fom j,bot says:"hi, ${msg.from.first_name}"')
+var api =  новая  телеграмма({
+ маркер:  '833678201:AAH0ZdPrbCeI4JN475Wl4zQl0KV84S6pvmc ',
+ обновления : {
+ включено:  true,
+ get_interval:  100
+             }
+});
+api.on ('сообщение ', функция (сообщение)
+{
+    var chat_id =  сообщение .болтовня .id;
+        консоль .log ("это идентификатор чата пользователя" + chat_id);
 
+api.sendPhoto({
+ chat_id:  сообщение .болтовня .id,
+ надпись :  "Это мой тестовый образ ",
+ фото :  https://core.telegram.org/file/811140217/1/NkRCCLeQZVc/17a804837802700ea4 ' / / замените url-адрес изображения здесь
 })
+.затем (функция (данные)
+{
+    консоль .журнал (данные);
+});
+});
